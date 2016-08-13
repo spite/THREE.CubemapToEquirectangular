5e5590634feaab18b3e412fdd8bacdec818da88e
Jaume Sanchez <the.spite@gmail.com>
2016-08-13 03:21:59 +0200

    added polyfill to ImageData

---
+++ examples/js/ImageData-polyfill.js
@@ -0,0 +1,15 @@
+( function() {
+
+	try {
+		var i = new ImageData( new Uint8ClampedArray( [ 1, 1, 1, 1 ] ), 1, 1 );
+	} catch( e ) {
+		window.ImageData = function( data, width, height ) {
+			var canvas = document.createElement('canvas');
+			var ctx = canvas.getContext( '2d' );
+			var imageData = canvas.getContext('2d').createImageData(width, height);
+			imageData.data.set(data);
+			return imageData
+		}
+	}
+
+})();
